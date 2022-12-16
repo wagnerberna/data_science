5f36e9d813ec60986f81b180f9e42e4e06087108
Wagner Berna <72299498+wagnerberna@users.noreply.github.com>
2022-12-16 08:31:20 -0300

    test

---
+++ tkinter/start.py
@@ -0,0 +1,27 @@
+import tkinter
+from tkinter import *
+
+
+class Application:
+    def __init__(self, master=None):
+        pass
+
+
+root = Tk()
+Application(root)
+
+root.title("Título Teste")
+
+# texto = Label(root, text="Insira aqui o seu texto do botão")
+
+# texto.grid(column=0, row=0, padx=10, pady=10)
+
+# botao = Button(root, text="Insira aqui a ação do seu botão", command=acao_botao)
+
+# botao.grid(column=0, row=1, padx=10, pady=10)
+
+# texto_resposta = Label(root, text="")
+
+# texto_resposta.grid(column=0, row=2, padx=10, pady=10)
+
+root.mainloop()
---
+++ tkinter/test.py
@@ -0,0 +1,7 @@
+import tkinter
+from tkinter import *
+
+root = Tk()
+
+
+root.title("Título Teste")
